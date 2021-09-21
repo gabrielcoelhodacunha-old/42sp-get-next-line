856baee6f7e3b80f28d634f0213a2d7a0dc20352
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2021-09-21 16:26:04 -0300

    Almost there...

--- get_next_line.c
+++ get_next_line.c
@@ -1,54 +1,62 @@
 #include "get_next_line.h"
+#include "../00-libft/libft.h"
 
 #include <stdio.h>
 
-ssize_t	fill_buffer(char **buffer, int fd);
-char	*get_content(char *buffer);
-
 char	*get_next_line(int fd)
 {
-	char	*buffer;
+	static char	*buffer;
+	char	*aux;
 	char	*line;
+	static ssize_t	bytes_read;
+	char	*new_line;
+	size_t	line_len;
 
-	bytes_read = fill_buffer(&buffer, fd);
-	line = get_content(buffer);
-	while (!ft_strrchr(line, '\n'))
+	if (!buffer)
+		buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	aux = NULL;
+	line = ft_strdup("");
+	if (!bytes_read || bytes_read == BUFFER_SIZE)
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (!bytes_read)
 	{
+		free(buffer);
+		free(line);
+		return (NULL);
 	}
-	free(buffer);
+	new_line = NULL;
+	while (bytes_read)
+	{
+		if (bytes_read < 0 || bytes_read > BUFFER_SIZE)
+		{
+			free(buffer);
+			free(aux);
+			free(line);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		
+		new_line = ft_strchr(buffer, '\n');
+		if (!new_line)
+			new_line = buffer + bytes_read;
+		line_len = new_line - buffer + 1;
+		aux = malloc(line_len + 1);
+		if (!aux)
+			return (NULL);
+		ft_memcpy(aux, buffer, line_len);
+		aux[line_len] = '\0';
+		line = ft_strjoin(line, aux);
+	
+		if (new_line)
+		{
+			bytes_read = 0;
+			continue ;
+		}
+
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		free(aux);
+	}
 	return (line);
-}
-
-char	*get_content(char *buffer)
-{
-	size_t	idx;
-	char	*content;
-
-	idx = -1;
-	while (buffer[++idx])
-		;
-	content = malloc((idx + 1) * sizeof(char));
-	if (!content)
-		return (NULL);
-	idx = -1;
-	while (buffer[++idx])
-		content[idx] = buffer[idx];
-	return (content);
-}
-
-ssize_t	fill_buffer(char **buffer, int fd)
-{
-	ssize_t	bytes_read;
-
-	*buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!*buffer)
-		return (NULL);
-	bytes_read = read(fd, *buffer, BUFFER_SIZE);
-	if (bytes_read <= 0 || bytes_read > BUFFER_SIZE)
-	{
-		free(*buffer);
-		return (NULL);
-	}
-	(*buffer)[bytes_read] = '\0';
-	return (bytes_read);
 }
